553889ba2c8ab11895a7234bff20182dc52acc03
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-01-29 22:57:46 +1000

    fix(views.py): replaced with current ones

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,11 +1,14 @@
+"""This module is described routing functions (control in MVC model)."""
+
 from django.shortcuts import render
 from django.http import Http404
+from blog.models import Post, Category, Location
 
 
 def index(request):
     template = 'blog/index.html'
     context = {
-        'posts': posts[::-1]
+        'post_list': Post.objects.all()
     }
     return render(request, template, context)
 
@@ -13,8 +16,7 @@ def index(request):
 def post_detail(request, post_id):
     template = 'blog/detail.html'
 
-    # Поиск поста по id
-    post = posts_dict.get(post_id)
+    post = Post.objects.get(id=post_id)
 
     # Если пост не найден, вызываем ошибку 404
     if post is None:
@@ -29,12 +31,12 @@ def post_detail(request, post_id):
 def category_posts(request, category_slug):
     template = 'blog/category.html'
 
-    filtered_posts = [post for post in posts
-                      if post['category'] == category_slug]
+    category_id = Category.objects.get(slug=category_slug).id
+    filtered_posts = Post.objects.filter(category=category_id)
 
     context = {
         'category': category_slug,
-        'posts': filtered_posts
+        'post_list': filtered_posts
     }
 
     return render(request, template, context)
