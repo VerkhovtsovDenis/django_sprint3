3c0781dfa5472e44bc9f75a0fe9e63448d411557
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-01-29 19:53:45 +1000

    feature(models.db) add describe of model

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField, TextField, DateTimeField, ForeignKey, BooleanField, SlugField
+from django.contrib.auth import get_user_model
 
 
-class Post(models.Model):
-    """Модель базы данных для объекта Публикация."""
-
-    pass
+# Модель базы данных для объекта Пользователь
+User = get_user_model()
 
 
 class Category(models.Model):
     """Модель базы данных для объекта Тематическая категория."""
+
+    title = CharField(max_length=256, blank=False)
+    description = TextField(blank=False)
+    slug = SlugField(unique=True, blank=False)
+    is_published = BooleanField(default=True, blank=False)
+    created_at = DateTimeField(auto_created=True, blank=False)
 
     pass
 
@@ -17,10 +22,24 @@ class Category(models.Model):
 class Location(models.Model):
     """Модель базы данных для объекта Географическая метка."""
 
+    name = CharField(max_length=256, blank=False)
+    is_published = BooleanField(default=True, blank=False)
+    created_at = DateTimeField(auto_created=True, blank=False)
+
     pass
 
 
-class User(AbstractUser):
-    """Модель базы данных для объекта Пользователь."""
+class Post(models.Model):
+    """Модель базы данных для объекта Публикация."""
+
+    title = CharField(max_length=256, blank=False)
+    text = TextField(blank=False)
+    pub_date = DateTimeField(blank=False)
+    author = CharField(max_length=256, blank=False)
+    author = ForeignKey(User, on_delete=models.CASCADE,
+                        related_name='fk_publications')
+    location = ForeignKey(Location, on_delete=models.SET_NULL, blank=True)
+    is_published = BooleanField(default=True, blank=False)
+    created_at = DateTimeField(auto_created=True, blank=False)
 
     pass
