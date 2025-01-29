797df3c0f714aa76bb76d490ffd6dc400b93429c
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-01-30 00:28:34 +1000

    feature(views.py): add control logic

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,26 +1,42 @@
 """This module is described routing functions (control in MVC model)."""
 
 from django.shortcuts import render
-from django.http import Http404
-from blog.models import Post, Category, Location
+from blog.models import Post, Category
+from datetime import datetime
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
+    """Возвращает главную страницу с инфрормацией о 5 записях."""
     template = 'blog/index.html'
+
+    published_category = Category.objects.filter(is_published=True)
+    today = datetime.today().date()
+
     context = {
-        'post_list': Post.objects.all()
+        'post_list': Post.objects.filter(
+                                         pub_date__lte=today,
+                                         is_published=True,
+                                         category__in=published_category
+        )[:5]
     }
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
+    """Возвращает страницу с постом по его id."""
     template = 'blog/detail.html'
 
-    post = Post.objects.get(id=post_id)
+    published_category = Category.objects.filter(is_published=True)
 
-    # Если пост не найден, вызываем ошибку 404
-    if post is None:
-        raise Http404(f"Post with {post_id=} does not exist")
+    today = datetime.today().date()
+    post = get_object_or_404(
+                             Post,
+                             id=post_id,
+                             is_published=True,
+                             pub_date__lte=today,
+                             category__in=published_category
+           )
 
     context = {
         'post': post
@@ -29,10 +45,19 @@ def post_detail(request, post_id):
 
 
 def category_posts(request, category_slug):
+    """Возвращает страницу категории по её slug."""
     template = 'blog/category.html'
 
-    category_id = Category.objects.get(slug=category_slug).id
-    filtered_posts = Post.objects.filter(category=category_id)
+    today = datetime.today().date()
+
+    category_id = get_object_or_404(Category,
+                                    slug=category_slug,
+                                    is_published=True).id
+    filtered_posts = Post.objects.filter(
+                                         category=category_id,
+                                         pub_date__lte=today,
+                                         is_published=True
+    )
 
     context = {
         'category': category_slug,
