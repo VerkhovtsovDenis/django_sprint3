42ed096b51b046946cf30fc53bfbc3e802f6e47e
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-01-30 00:49:37 +1000

    style
    	modified:   blogicum/blog/models.py
    
    	modified:   blogicum/blog/models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -15,25 +15,20 @@ class Category(models.Model):
 
     title = CharField(max_length=256, blank=False, verbose_name='Заголовок')
     description = TextField(blank=False, verbose_name='Описание')
-    slug = SlugField(
-                     unique=True,
+    slug = SlugField(unique=True,
                      blank=False,
                      verbose_name='Идентификатор',
                      help_text='Идентификатор страницы для URL; разрешены \
 символы латиницы, цифры, дефис и подчёркивание.')
 
-    is_published = BooleanField(
-                                default=True,
+    is_published = BooleanField(default=True,
                                 blank=False,
                                 verbose_name='Опубликовано',
                                 help_text='Снимите галочку, \
-чтобы скрыть публикацию.'
-    )
-    created_at = DateTimeField(
-                               blank=False,
+чтобы скрыть публикацию.')
+    created_at = DateTimeField(blank=False,
                                verbose_name='Добавлено',
-                               auto_now_add=True
-    )
+                               auto_now_add=True)
 
     class Meta:
         """Зарещенный на територии РФ класс, \
@@ -50,21 +45,15 @@ class Category(models.Model):
 class Location(models.Model):
     """Модель базы данных для объекта Географическая метка."""
 
-    name = CharField(
-                     max_length=256,
+    name = CharField(max_length=256,
                      blank=False,
-                     verbose_name='Название места'
-    )
-    is_published = BooleanField(
-                                default=True,
+                     verbose_name='Название места')
+    is_published = BooleanField(default=True,
                                 blank=False,
-                                verbose_name='Опубликовано'
-    )
-    created_at = DateTimeField(
-                               blank=False,
+                                verbose_name='Опубликовано')
+    created_at = DateTimeField(blank=False,
                                verbose_name='Добавлено',
-                               auto_now_add=True
-    )
+                               auto_now_add=True)
 
     class Meta:
         """Зарещенный на територии РФ класс, \
@@ -83,46 +72,34 @@ class Post(models.Model):
 
     title = CharField(max_length=256, blank=False, verbose_name='Заголовок')
     text = TextField(blank=False, verbose_name='Текст')
-    pub_date = DateTimeField(
-                             blank=False,
+    pub_date = DateTimeField(blank=False,
                              verbose_name='Дата и время публикации',
                              help_text='Если установить дату и время в \
-будущем — можно делать отложенные публикации.'
-    )
-    author = ForeignKey(
-                        User,
+будущем — можно делать отложенные публикации.')
+    author = ForeignKey(User,
                         on_delete=models.CASCADE,
                         related_name='fk_author',
                         blank=False,
-                        verbose_name='Автор публикации'
-    )
-    location = ForeignKey(
-                          Location,
+                        verbose_name='Автор публикации')
+    location = ForeignKey(Location,
                           on_delete=models.SET_NULL,
                           related_name='fk_location',
                           blank=False,
                           null=True,
-                          verbose_name='Местоположение'
-    )
-    category = ForeignKey(
-                          Category,
+                          verbose_name='Местоположение')
+    category = ForeignKey(Category,
                           on_delete=models.SET_NULL,
                           related_name='fk_category',
                           blank=False,
                           null=True,
-                          verbose_name='Категория'
-    )
-    is_published = BooleanField(
-                                default=True,
+                          verbose_name='Категория')
+    is_published = BooleanField(default=True,
                                 blank=False,
-                                verbose_name='Опубликовано'
-    )
-    created_at = DateTimeField(
-                               auto_created=True,
+                                verbose_name='Опубликовано')
+    created_at = DateTimeField(auto_created=True,
                                blank=False,
                                verbose_name='Добавлено',
-                               auto_now_add=True
-    )
+                               auto_now_add=True)
 
     class Meta:
         """Зарещенный на територии РФ класс, \
